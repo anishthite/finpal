d2d2214827e26dcf13c6ff2f4c9caa8792f8d288
reagankan <rkan3@gatech.edu>
2019-10-26 20:43:13 -0400

    func to convert to riskyness

--- app.py
+++ app.py
@@ -10,6 +10,31 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
+def f(l):
+    """
+    Args:
+        seq[str] : sequence of user response to 13 questions
+    Returns:
+        str : risky-ness. either: "L", "M", "H"
+    """
+    assert(len(l) == 13), f"expecting 13 entries, but actual: {len(l)}"
+    accum = 0
+    one = {"a":4, "b":3, "c":2, "d":1}
+    diff = {"a":1, "b":3, "c":0, "d":0}
+    reg = {"a":1, "b":3, "c":3, "d":4}
+    for i, val in enumerate(l):
+        if i == 0:
+            accum += one[val]
+        elif i == 8 or i == 9:
+            accum += diff[val]
+        else:
+            accum += reg[val]
+    if accum < 21:
+        return "L"
+    elif 21 <= accum <= 30:
+        return "M"
+    else:
+        return "H"
 
 # create a route for webhook
 @app.route('/webhook', methods=['POST'])
